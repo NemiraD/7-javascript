8f8059648ea26c90611465aedc4edfcf4b364acc
Nemira <nemira.dirmontaite@gmail.com>
2019-09-11 20:41:08 +0300

    main

---
+++ js/maain.js
@@ -0,0 +1,34 @@
+console.log(2019);
+
+console.log('jgfjfk');
+
+console.log('jgfjfk 2019', 20156, 'kzjvbzkv', 4587);
+
+console.log(true);
+
+console.log(false);
+
+console.log('------------');
+
+var vardas = 'Nemira';
+console.log(vardas);
+
+var amzius = 15;
+console.log(amzius);
+
+var arAmziusTikras = false;
+console.log(arAmziusTikras);
+
+let svecioVardas = 'Adomas';
+console.log(svecioVardas);
+
+svecioVardas = 'Ieva';
+console.log(svecioVardas);
+
+//type:array
+const laimingiSkaiciuSarasas = [1, 5, 7, 8, 9];
+console.log(laimingiSkaiciuSarasas);
+
+const raides = ['a', 'b', 'Ą'];
+console.log(raides);
+
